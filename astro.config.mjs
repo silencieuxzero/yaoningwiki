813cbdd87703dedecbc4aed2e7a350fe404458c2
“silencieux” <rain_wind1217@outlook.com>
2025-07-28 22:12:08 +0800

    添加内容

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,23 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: '药宁的服务器wiki',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: '游玩准备',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: '准备工作', slug: 'guides/start' },
 					],
 				},
 				{
-					label: 'Reference',
+					label: '服务器内建筑/设施坐标',
 					autogenerate: { directory: 'reference' },
+				},
+								{
+					label: '提问与回答',
+					autogenerate: { directory: 'qa' },
 				},
 			],
 		}),
